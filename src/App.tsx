bd29ef6369640c71473fc5ac45e80b48b7c3918b
Dmitriy24S <devilins24@gmail.com>
2023-01-28 20:34:59 +0200

    feat: add loading skeleton for intro img

--- src/App.tsx
+++ src/App.tsx
@@ -179,7 +179,7 @@ function App() {
       {status === 'idle' && (
         <div className='flex flex-col justify-center items-center gap-4'>
           <div className='random-image-container min-h-[300px] max-h-[300px] md:min-h-[500px] md:max-h-[500px] w-full rounded-md overflow-hidden mb-4'>
-            {/* Image */}
+            {/* Intro Image */}
             {randomImgFetchStatus === 'success' && (
               <img
                 src={randomImg}
@@ -187,6 +187,10 @@ function App() {
                 className='w-full object-cover object-center cursor-pointer block md:min-h-[500px] md:max-h-[500px]'
                 onClick={() => randomImgRefetch()}
               />
+            )}
+            {/* Intro Image - Loading skeleton  */}
+            {randomImgFetchStatus === 'loading' && (
+              <div className='animate-pulse min-h-full max-h-[300px] md:min-h-[500px] md:max-h-[500px] bg-neutral-700'></div>
             )}
           </div>
           <h2 className='text-xl'>Want to see a random meal?</h2>
